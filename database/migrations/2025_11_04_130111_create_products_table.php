ba0fbe836c3cd206db9ffca1eeea9c327b260255
kovacsnandor <kovacsnandor@gmail.com>
2025-11-05 09:34:46 +0100

    Migráció kész

--- database/migrations/2025_11_04_130111_create_products_table.php
+++ database/migrations/2025_11_04_130111_create_products_table.php
@@ -12,8 +12,36 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->integer('id')->autoIncrement();
+            $table->primary('id');
+            $table->string('category', 255)->notNull();
+            $table->string('name', 191)->notNull();
+            $table->string('description', 255);
+            $table->string('picture', 255);
+            $table->Integer('price');
+            $table->Integer('stock');
+            //Minta mezők
+
+            // --- 2. Logikai (BOOLEAN) Alapértelmezett Értékkel
+            // Alapértelmezés: FALSE (0)
+            // TINYINT(1)
+            $table->boolean('is_published')->default(false);
+
+            // --- 3. Dátum (DATE)
+            $table->date('start_date')->nullable()->default(null);
+
+            // --- 4. Dátum és Idő (DATETIME) Alapértelmezett Értékkel
+            // Alapértelmezés: NULL
+            $table->dateTime('scheduled_posting')->nullable()->default(null);
+
+            // --- 5. Idő (TIME)
+            // Alapértelmezés: '09:00:00'
+            $table->time('delivery_time')->default('09:00:00');
+
+            // --- 6. Decimális (DECIMAL) Alapértelmezett Értékkel
+            // Ár oszlop: 10 számjegy összesen, 2 tizedesjegy pontossággal.
+            // Alapértelmezés: 0.00
+            $table->decimal('final_price', 10, 2)->default(0.00);
         });
     }
 
