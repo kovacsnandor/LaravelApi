9eeafed369aa1c196d5f3bbacfb95d56852b420d
kovacsnandor <kovacsnandor@gmail.com>
2025-11-06 10:14:04 +0100

    Saját tömb kész

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -13,21 +14,109 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        //SQL
-        $sql ="INSERT INTO `products` (`id`, `category`, `name`, `description`, `picture`, `price`, `stock`) VALUES
-            (1, 'Bogyós', 'Málna', 'Kézzel termelt egészség', 'https://hur.webmania.cc/img/malna.jpg', 3800, 500),
-            (2, 'Bogyós', 'Áfonya', 'Az erdő kincse az otthonodba', 'https://hur.webmania.cc/img/afonya.jpg', 3250, 120),
-            (3, 'Bogyós', 'Szeder', 'A hagyományos csemege', 'https://hur.webmania.cc/img/szeder.jpg', 1700, 40),
-            (4, 'Bogyós', 'Eper', 'Egy tavaszi harapás', 'https://hur.webmania.cc/img/eper.jpg', 1440, 0),
-            (5, 'Bogyós', 'Homoktövis', 'Mezei csemege', 'https://hur.webmania.cc/img/homoktovis.jpg', 3200, 100),
-            (6, 'Bogyós', 'Som', 'A fanyar gyönyör', 'https://hur.webmania.cc/img/som.jpg', 900, 10),
-            (7, 'Bogyós', 'Fanyarka', 'Édes mint a méz', 'https://hur.webmania.cc/img/fanyarka.jpg', 990, 25),
-            (8, 'Bogyós', 'Piszke', 'Egres', 'https://hur.webmania.cc/img/piszke.jpg', 750, 100),
-            (9, 'Bogyós', 'Ribizli', 'Fanyar, vasban gazdag', 'https://hur.webmania.cc/img/ribizli.jpg', 1300, 170),
-            (10, 'Magyaros', 'Meggy', 'A falusi kincs', 'https://hur.webmania.cc/img/meggy.jpg', 600, 300),
-            (11, 'Magyaros', 'Cseresznye', 'A falusi kincs', 'https://hur.webmania.cc/img/cseresznye.jpg', 900, 300),
-            (12, 'Magyaros', 'Szilva', 'A falusi kincs', 'https://hur.webmania.cc/img/szilva.jpg', 770, 200)
-        ";
-        DB::statement($sql);
+        //Seedelés tömbbel.
+       $data = 
+       [
+            [
+                'category' => 'Bogyós',
+                'name' => 'Málna2',
+                'description' => 'Kézzel termelt egészség',
+                'picture' => 'https://hur.webmania.cc/img/malna.jpg',
+                'price' => 3800,
+                'stock' => 500,
+            ],
+            [
+                'category' => 'Bogyós',
+                'name' => 'Áfonya',
+                'description' => 'Az erdő kincse az otthonodba',
+                'picture' => 'https://hur.webmania.cc/img/afonya.jpg',
+                'price' => 3250,
+                'stock' => 120,
+            ],
+            [
+                'category' => 'Bogyós',
+                'name' => 'Szeder',
+                'description' => 'A hagyományos csemege',
+                'picture' => 'https://hur.webmania.cc/img/szeder.jpg',
+                'price' => 1700,
+                'stock' => 40,
+            ],
+            [
+                'category' => 'Bogyós',
+                'name' => 'Eper',
+                'description' => 'Egy tavaszi harapás',
+                'picture' => 'https://hur.webmania.cc/img/eper.jpg',
+                'price' => 1440,
+                'stock' => 0,
+            ],
+            [
+                'category' => 'Bogyós',
+                'name' => 'Homoktövis',
+                'description' => 'Mezei csemege',
+                'picture' => 'https://hur.webmania.cc/img/homoktovis.jpg',
+                'price' => 3200,
+                'stock' => 100,
+            ],
+            [
+                'category' => 'Bogyós',
+                'name' => 'Som',
+                'description' => 'A fanyar gyönyör',
+                'picture' => 'https://hur.webmania.cc/img/som.jpg',
+                'price' => 900,
+                'stock' => 10,
+            ],
+            [
+                'category' => 'Bogyós',
+                'name' => 'Fanyarka',
+                'description' => 'Édes mint a méz',
+                'picture' => 'https://hur.webmania.cc/img/fanyarka.jpg',
+                'price' => 990,
+                'stock' => 25,
+            ],
+            [
+                'category' => 'Bogyós',
+                'name' => 'Piszke',
+                'description' => 'Egres',
+                'picture' => 'https://hur.webmania.cc/img/piszke.jpg',
+                'price' => 750,
+                'stock' => 100,
+            ],
+            [
+                'category' => 'Bogyós',
+                'name' => 'Ribizli',
+                'description' => 'Fanyar, vasban gazdag',
+                'picture' => 'https://hur.webmania.cc/img/ribizli.jpg',
+                'price' => 1300,
+                'stock' => 170,
+            ],
+            [
+                'category' => 'Magyaros',
+                'name' => 'Meggy',
+                'description' => 'A falusi kincs',
+                'picture' => 'https://hur.webmania.cc/img/meggy.jpg',
+                'price' => 600,
+                'stock' => 300,
+            ],
+            [
+                'category' => 'Magyaros',
+                'name' => 'Cseresznye',
+                'description' => 'A falusi kincs',
+                'picture' => 'https://hur.webmania.cc/img/cseresznye.jpg',
+                'price' => 900,
+                'stock' => 300,
+            ],
+            [
+                'category' => 'Magyaros',
+                'name' => 'Szilva',
+                'description' => 'A falusi kincs',
+                'picture' => 'https://hur.webmania.cc/img/szilva.jpg',
+                'price' => 770,
+                'stock' => 200,
+            ],
+        ];
+       
+        if (Product::count() === 0) {
+            Product::factory()->createMany($data);
+        }
     }
 }
