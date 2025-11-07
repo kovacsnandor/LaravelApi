f3138b52ab1d83b45217bc96b0bd21fd5584e67f
kovacsnandor <kovacsnandor@gmail.com>
2025-11-07 10:16:24 +0100

    feladat kész

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -9,6 +9,23 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class ProductFactory extends Factory
 {
+
+    protected array $productData = [
+        ['category' => 'Bogyós', 'name' => 'Málna', 'description' => 'Kézzel termelt egészség', 'picture' => 'https://hur.webmania.cc/img/malna.jpg'],
+        ['category' => 'Bogyós', 'name' => 'Áfonya', 'description' => 'Az erdő kincse az otthonodba', 'picture' => 'https://hur.webmania.cc/img/afonya.jpg'],
+        ['category' => 'Bogyós', 'name' => 'Szeder', 'description' => 'A hagyományos csemege', 'picture' => 'https://hur.webmania.cc/img/szeder.jpg'],
+        ['category' => 'Bogyós', 'name' => 'Eper', 'description' => 'Egy tavaszi harapás', 'picture' => 'https://hur.webmania.cc/img/eper.jpg'],
+        ['category' => 'Bogyós', 'name' => 'Homoktövis', 'description' => 'Mezei csemege', 'picture' => 'https://hur.webmania.cc/img/homoktovis.jpg'],
+        ['category' => 'Bogyós', 'name' => 'Som', 'description' => 'A fanyar gyönyör', 'picture' => 'https://hur.webmania.cc/img/som.jpg'],
+        ['category' => 'Bogyós', 'name' => 'Fanyarka', 'description' => 'Édes mint a méz', 'picture' => 'https://hur.webmania.cc/img/fanyarka.jpg'],
+        ['category' => 'Bogyós', 'name' => 'Piszke', 'description' => 'Egres', 'picture' => 'https://hur.webmania.cc/img/piszke.jpg'],
+        ['category' => 'Bogyós', 'name' => 'Ribizli', 'description' => 'Fanyar, vasban gazdag', 'picture' => 'https://hur.webmania.cc/img/ribizli.jpg'],
+        ['category' => 'Magyaros', 'name' => 'Meggy', 'description' => 'A falusi kincs', 'picture' => 'https://hur.webmania.cc/img/meggy.jpg'],
+        ['category' => 'Magyaros', 'name' => 'Cseresznye', 'description' => 'A falusi kincs', 'picture' => 'https://hur.webmania.cc/img/cseresznye.jpg'],
+        ['category' => 'Magyaros', 'name' => 'Szilva', 'description' => 'A falusi kincs', 'picture' => 'https://hur.webmania.cc/img/szilva.jpg'],
+    ];
+
+
     /**
      * Define the model's default state.
      *
@@ -16,8 +33,21 @@ class ProductFactory extends Factory
      */
     public function definition(): array
     {
+        // Véletlenszerűen kiválasztunk egy elemet a $productData tömbből
+        $randomProduct = $this->faker->randomElement($this->productData);
+        $randomUniqueNumber = $this->faker->unique()->randomNumber(6, true);
+
         return [
-            //
+            //         // Fix adatok a kiválasztott elemből
+            'category' => $randomProduct['category'],
+            //'name' => $randomProduct['name'] . ' (' . $this->faker->unique()->city() . ')', // Némi variáció hozzáadása
+            'name' =>$randomProduct['name'] . ' ('.$randomUniqueNumber.')', // Némi variáció hozzáadása
+            'description' => $randomProduct['description'],
+            'picture' => $randomProduct['picture'],
+
+            // Véletlen adatok a Faker segítségével
+            'price' => $this->faker->numberBetween(500, 5000), // Véletlen ár 500 és 5000 között
+            'stock' => $this->faker->numberBetween(0, 1000),   // Véletlen raktárkészlet 0 és 1000 között
         ];
     }
 }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -14,51 +14,7 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        //Seedelés tömbbel.
-        $data = [];
-        // $fileNameCsv = database_path('csv/products.csv');
-
-        // $rows = file($fileNameCsv, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-
-        // for ($i = 1; $i < count($rows); $i++) {
-        //     $cols = explode(';', $rows[$i]);
-
-        //     $data[] = [
-        //         'category' => $cols[0],
-        //         'name' => $cols[1],
-        //         'description' => $cols[2],
-        //         'picture' => $cols[3],
-        //         'price' => (int)$cols[4],
-        //         'stock' => (int)$cols[5]
-        //     ];
-        // }
-
-        // var_dump($data);
-        // die;
-        //bolvasás
-
-        //Profibb megoldás (nagyon nagy fájlok esetén):
-        $filePath = database_path('csv/products.csv');
-        $data = [];
-        $header = []; // Fejlécek tárolására
-
-        if (($handle = fopen($filePath, 'r')) !== false) {
-            // 1. Beolvassuk a fejléceket (ha vannak)
-            $header = fgetcsv($handle, 0, ';');
-
-            // 2. Soronként beolvassuk az adatokat (0 azt jelenti, hogy nincs korlát a beolvasott sorra)
-            while (($cols = fgetcsv($handle, 0, ';')) !== false) {
-                if (count($header) === count($cols)) {
-                    // Asszociatív tömb létrehozása (jobb olvashatóság!)
-                    $data[] = array_combine($header, $cols);
-                }
-            }
-            // 3. Zárjuk a fájlt (itt kötelező!)
-            fclose($handle);
-        }
-
-        if (Product::count() === 0) {
-            Product::factory()->createMany($data);
-        }
+        ini_set('memory_limit', '256M');
+        Product::factory()->count(10000)->create();
     }
 }
