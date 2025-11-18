1691798ebd5836021b904d5e65473934ea8788ba
kovacsnandor <kovacsnandor@gmail.com>
2025-11-18 11:21:23 +0100

    AppServiceProvider regisztráció

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,10 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Route; // <--- EZT KELL HOZZÁADNI
+use Laravel\Sanctum\Http\Middleware\CheckAbilities; // <--- EZT KELL HOZZÁADNI
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Illuminate\Support\Facades\Exceptions;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +28,19 @@ class AppServiceProvider extends ServiceProvider
         //
         // Az alapértelmezett string hossza 191 karakterre csökkentése
         Schema::defaultStringLength(191);
+        //Middleware regisztráció
+        Route::aliasMiddleware('ability', CheckAbilities::class);
+
+        // 2. KIVÉTELKEZELÉS REGISZTRÁCIÓJA
+        Exceptions::renderable(function (AccessDeniedHttpException $e, $request) {
+
+            // Csak API kérésekre fusson le
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Access denied.'
+                ], 403);
+            }
+        });
         
     }
 }
