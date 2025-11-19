4f6485d88907e0a7c8caecf930772fc1e2ef39b7
kovacsnandor <kovacsnandor@gmail.com>
2025-11-19 10:10:10 +0100

    AppServiceProvider policy regisztráció, hibüzenet kezelés

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use App\Policies\UserPolicy;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Response;
@@ -9,6 +11,7 @@ use Illuminate\Support\Facades\Route; // <--- EZT KELL HOZZÁADNI
 use Laravel\Sanctum\Http\Middleware\CheckAbilities; // <--- EZT KELL HOZZÁADNI
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Illuminate\Support\Facades\Exceptions;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -33,14 +36,17 @@ class AppServiceProvider extends ServiceProvider
 
         // 2. KIVÉTELKEZELÉS REGISZTRÁCIÓJA
         Exceptions::renderable(function (AccessDeniedHttpException $e, $request) {
-
+            $message = $e->getMessage() ?? 'Access denied.';
             // Csak API kérésekre fusson le
             if ($request->is('api/*')) {
                 return response()->json([
-                    'message' => 'Access denied.'
+                    'message' => $message
                 ], 403);
             }
         });
+
+        //Policy regisztráció
+        Gate::policy(User::class, UserPolicy::class);
         
     }
 }
